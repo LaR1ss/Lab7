c745c7fc649cfd75feb31b6c86ca7be3788ee114
LaR1ss <157617797+LaR1ss@users.noreply.github.com>
2024-03-25 17:12:26 +0600

    Delete tupl.py

--- tupl.py
+++
@@ -1,4 +0,0 @@
-a = ("apple", "banana", "cherry", "LoL", "Kekw", "Wtf", "Hello", "are", "Thanks", "OMG", "Oh no", "I", "Love", "You", "Sky")
-print(len(a))
-b = ("apple", 25, "cherry", True )
-print(type(b))
